d9bd19d13048b96b642e32443ac89956923b33d1
ppilatso <pilatsophenyo@gmail.com>
2022-11-10 18:29:37 +0200

    Display List Data

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,12 +1,11 @@
 import { MOCK_PROJECTS } from './MockProjects'
+import ProjectList from './ProjectList'
 
-type Props = {}
-
-function ProjectsPage(props: Props) {
+function ProjectsPage() {
   return (
     <>
       <h1>Projects</h1>
-      <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+      <ProjectList projects={MOCK_PROJECTS} />
     </>
   )
 }
